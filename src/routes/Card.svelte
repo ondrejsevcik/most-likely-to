6564e2d731fa4138792353f064184c811e159606
Ondrej Sevcik <hi@ondrejsevcik.com>
2023-02-26 17:01:24 +0100

    Fix layout

--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -11,32 +11,46 @@
 
 <style>
 	.card {
-		background-color: white;
 		border-radius: 10px;
+		background-color: white;
+		color: var(--color-text-dark);
 
-		max-width: 320px;
 		display: flex;
 		flex-direction: column;
-		justify-content: flex-end;
+		justify-content: space-between;
+		height: 70vh;
+		max-height: 600px;
+		width: 100%;
+		max-width: 500px;
 
-		color: var(--color-text-dark);
 		padding: 1rem;
+		padding-top: 3rem;
+		gap: 0.5rem;
 	}
 
 	.question {
-		/* TODO change */
 		font-family: 'Raleway';
 		font-weight: 500;
-		font-size: 44px;
+		font-size: 2.75rem;
 		line-height: 1.2;
+	}
 
-		/* TODO align to center always - use variable fonts */
-		margin-bottom: 3rem;
+	@media screen and (max-height: 400px) {
+		.card {
+			padding: 1rem;
+			height: initial;
+		}
+
+		.question {
+			/* TODO use variable fonts */
+			font-size: 2rem;
+		}
 	}
 
 	.btn-next {
 		border: 2px solid var(--color-text-dark);
 		background-color: white;
+		color: var(--color-text-dark);
 
 		font-family: Inter;
 		font-weight: 600;
